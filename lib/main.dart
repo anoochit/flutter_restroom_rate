35452c6226e563495da663ae530d69405961b9de
Anuchit Chalothorn <anoochit@gmail.com>
2019-12-19 10:40:38 +0700

    update layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,77 +1,113 @@
 import 'package:flutter/material.dart';
-import 'package:smooth_star_rating/smooth_star_rating.dart';
+import 'package:restroom_rate/thankyou.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  MyApp({Key key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  double rating = 0;
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Restroom Rate',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-      ),
-      home: Scaffold(
-        body: Container(
-          // Add box decoration
-          decoration: BoxDecoration(
-            // Box decoration takes a gradient
-            gradient: LinearGradient(
-              // Where the linear gradient begins and ends
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              // Add one stop for each color. Stops should increase from 0 to 1
-              stops: [0.1, 0.9],
-              colors: [
-                // Colors are easy thanks to Flutter's Colors class.
-                Colors.pink[800],
-                Colors.purple[900],
-              ],
-            ),
+      title: 'Material App',
+      home: RateForm(),
+    );
+  }
+}
+
+class RateForm extends StatelessWidget {
+  const RateForm({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        // Add box decoration
+        decoration: BoxDecoration(
+          // Box decoration takes a gradient
+          gradient: LinearGradient(
+            // Where the linear gradient begins and ends
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            // Add one stop for each color. Stops should increase from 0 to 1
+            stops: [0.1, 0.9],
+            colors: [
+              // Colors are easy thanks to Flutter's Colors class.
+              Colors.pink[800],
+              Colors.purple[900],
+            ],
           ),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  "Good Afternoon",
-                  style: TextStyle(
-                      fontSize: 42,
-                      fontWeight: FontWeight.w700,
-                      color: Colors.white),
-                ),
-                Text(
-                  "Please rate our restroom",
-                  style: TextStyle(
-                      fontSize: 42,
-                      fontWeight: FontWeight.w700,
-                      color: Colors.white),
-                ),
-                SizedBox(
-                  height: 32.0,
-                ),
-                SmoothStarRating(
-                    allowHalfRating: false,
-                    onRatingChanged: (v) {
-                      rating = v;
-                      setState(() {});
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Good Morning",
+                style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white),
+              ),
+              Text(
+                "Please rate our restroom",
+                style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white),
+              ),
+              SizedBox(
+                height: 32.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.sentiment_very_dissatisfied),
+                    iconSize: 100.0,
+                    color: Colors.yellow[600],
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.sentiment_dissatisfied),
+                    iconSize: 100.0,
+                    color: Colors.yellow[600],
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.sentiment_neutral),
+                    iconSize: 100.0,
+                    color: Colors.yellow[600],
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => ThankyouPage()),
+                      );
                     },
-                    starCount: 5,
-                    rating: rating,
-                    size: 120.0,
-                    color: Colors.yellow,
-                    borderColor: Colors.yellow,
-                    spacing: 0.0)
-              ],
-            ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.sentiment_satisfied),
+                    iconSize: 100.0,
+                    color: Colors.yellow[600],
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => ThankyouPage()),
+                      );
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.sentiment_very_satisfied),
+                    iconSize: 100.0,
+                    color: Colors.yellow[600],
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => ThankyouPage()),
+                      );
+                    },
+                  ),
+                ],
+              )
+            ],
           ),
         ),
       ),
---
+++ lib/thankyou.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class ThankyouPage extends StatefulWidget {
+  ThankyouPage({Key key}) : super(key: key);
+
+  @override
+  _ThankyouPageState createState() => _ThankyouPageState();
+}
+
+class _ThankyouPageState extends State<ThankyouPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Thankyou Page',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: Scaffold(
+        body: Container(
+          // Add box decoration
+          decoration: BoxDecoration(
+            // Box decoration takes a gradient
+            gradient: LinearGradient(
+              // Where the linear gradient begins and ends
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              // Add one stop for each color. Stops should increase from 0 to 1
+              stops: [0.1, 0.9],
+              colors: [
+                // Colors are easy thanks to Flutter's Colors class.
+                Colors.pink[800],
+                Colors.purple[900],
+              ],
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Thank you",
+                  style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white),
+                ),
+                SizedBox(
+                  height: 32.0,
+                ),
+                RaisedButton(
+                  textColor: Colors.white,
+                  color: Colors.black,
+                  child: Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      "Close",
+                      style: TextStyle(fontSize: 22),
+                    ),
+                  ),
+                  shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(30.0),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
